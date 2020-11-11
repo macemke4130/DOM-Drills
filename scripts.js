a318a9d933f0a24d2a8a9f6843ff409da1043101
Lucas Mace <lucasmace4130@gmail.com>
2020-11-11 09:49:10 -0600

    Final Commit

--- scripts.js
+++ scripts.js
@@ -1,41 +1,61 @@
+// Initial State Variables --
+
+// Array containing "random" colors --
+let colors = ["red", "green", "blue", "yellow", "pink", "black"];
+
+// Counter for <li> items --
+let myCount = 1;
+
+// Creates <div> with a class of header-contianer --
 let headerDiv = document.createElement("div");
 headerDiv.className = "header-container";
+
+// Places the <div> in the HTML body tag --
 document.body.appendChild(headerDiv);
 
+// Puts 6 <h> tags on the screen in decending size --
 for (let index = 1; index <= 6; index++) {
     let h = document.createElement("h" + index);
     let hTxt = document.createTextNode("This is an h" + index);
     h.appendChild(hTxt);
-    h.className = "h" + index;
-    headerDiv.appendChild(h);
+    h.className = "h" + index; // <h1>, <h2>, ect --
+    headerDiv.appendChild(h); // Puts the <h> tag inside the <div> --
 
-    h.addEventListener("dblclick", function(){
+    // Double Click on any individual <h> tag to change the font color --
+    h.addEventListener("dblclick", function () {
         h.style.color = colors[ranColor()];
     })
 }
 
-function ranColor(){
-    let x = Math.floor(Math.random() * colors.length + 1) - 1;
-
+// Returns a random number between and including 0 and 6 --
+function ranColor() {
+    let x = Math.floor(Math.random() * colors.length);
     return x;
 }
 
-let colors = ["red", "green", "blue", "yellow", "pink", "black"];
-
+// Creates <ul> and appends to the <body> --
 let ul = document.createElement("ul");
 document.body.appendChild(ul);
 
-let myCount = 1;
 
-function addListItem(){
+// Adds <li> items to the <ul> on button click --
+function addListItem() {
     let li = document.createElement("li");
-    let liTxt = document.createTextNode("This is list item " + myCount)
+    let liTxt = document.createTextNode("This is list item " + myCount) // myCount is set to 1 initially --
     li.appendChild(liTxt);
 
-    ul.appendChild(li);
-    myCount++;
+    li.addEventListener("click", function () { // Selects random color from colors array --
+        li.style.color = colors[ranColor()];
+    });
+    li.addEventListener("dblclick", function () { // Removes <li> on a double click --
+        li.remove();
+    });
+
+    ul.appendChild(li); // Appends <li> to the <ul> --
+    myCount++; // Increment <li> --
 }
 
-document.getElementsByClassName("btn")[0].onclick = function(){
+// Listener for button click to create <li> items --
+document.getElementsByClassName("btn")[0].onclick = function () {
     addListItem();
 };
